d4fa2fca20cfd2541a4181ece2bf1c26eb0ba12f
arijitskiplegal <arijit@skiplegal.com>
2025-09-09 21:47:52 +0530

    Fix: Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 /** @type {import('vite').UserConfig} */
 export default {
   plugins: [react()],
-  base: '',
+  base: '/arijitmukherjee.github.io/',
   build: {
     outDir: '.',
     emptyOutDir: false,
